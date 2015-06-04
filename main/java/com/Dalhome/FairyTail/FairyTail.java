911ac0ae68dd46be0c0828424b235cb86983217e
Dalhome3 <da.ledger01@gmail.com>
2015-06-04 18:17:29 +0100

    Mod Start

---
+++ main/java/com/Dalhome/FairyTail/FairyTail.java
@@ -0,0 +1,37 @@
+package com.Dalhome.FairyTail;
+
+import com.Dalhome.FairyTail.Reference.Reference;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Reference.MODID, version = Reference.VERSION, name = Reference.NAME)
+public class FairyTail
+{
+    @EventHandler
+    public void preinit(FMLPreInitializationEvent event)
+    {
+
+
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+
+
+    }
+
+    @EventHandler
+    public void postinit(FMLPostInitializationEvent event)
+    {
+
+
+
+    }
+}
---
+++ main/java/com/Dalhome/FairyTail/Reference/Reference.java
@@ -0,0 +1,8 @@
+package com.Dalhome.FairyTail.Reference;
+
+
+public class Reference {
+    public static final String MODID = "fairytail";
+    public static final String NAME = "Fairy Tail";
+    public static final String VERSION = "1.0";
+}
